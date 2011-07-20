55fea2ed3b9be17a825724dab19c0ee2ae6bdcca
Raul <raulgonzalezgutierrez10@gmail.com>
2011-07-20 09:58:24 -0500

    Correciones en el controlador de imagenes

--- app/controllers/images_controller.php
+++ app/controllers/images_controller.php
@@ -56,7 +56,10 @@ class ImagesController extends AppController {
 								'modified_user_id' => $this->data['Image']['modified_user_id']
 								 
 								));
+					Debug($data2);
+								
 					$this->Image->save($data2);
+					
 					$this->data['Image']['url']['name'] = $data2['Image']['url'];
 					$this->upload($this->data);
 					$this->Session->setFlash(__('La imagen Guardada Exitosamente', true));
@@ -154,6 +157,8 @@ class ImagesController extends AppController {
 			$folder = Inflector::slug($folder['Tipoimage']['title'],'_');
 			$path = getcwd().'\\img\\'.$folder.'\\'.$image;
 			$path2 = getcwd().'\\img\\'.$folder.'\\'.$miniImage;
+			
+			
 			
 			if(!file_exists($path))
 			{
